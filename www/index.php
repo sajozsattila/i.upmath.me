802a84498da50cbca10c62e7c51f7681d0d48e73
Sajó Zsolt Attila <sajozsattila@gmail.com>
2021-01-20 21:00:26 +0000

    fix language

--- www/index.php
+++ www/index.php
@@ -11,7 +11,7 @@ if (!defined('FINGERPRINT')) {
 $service_url = '//mur2.co.uk/math/';
 $script_url = $service_url.'latex.js';
 
-$lang = $_SERVER['HTTP_HOST'] === 'en';
+$lang = 'en';
 $i18n = include 'lang.' . $lang . '.php';
 
 $langLinks = [
